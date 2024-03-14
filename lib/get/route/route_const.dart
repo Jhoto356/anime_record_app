975d12031b00e84d605b903ba47d7f4630a1d796
Jhoto356 <jhotocastro356@gmail.com>
2024-03-14 16:32:41 -0500

    Feature: File route_const.dart

---
+++ lib/get/route/route_const.dart
@@ -0,0 +1,9 @@
+import 'package:collection_app/ui/ui.dart';
+import 'package:get/get.dart';
+
+import '../../utils/utils.dart';
+
+class RouteConst {
+  static PairData loginPage = PairData(first: "/login", second: Get.find<LoginScreen>());
+
+}
